38473eaf03ec8950a2647d2789dfeff4169beac7
Flynn <FlynnSG10@uww.edu>
2021-09-28 13:11:17 -0500

    add value_count function

---
+++ Part-1.py
@@ -0,0 +1,7 @@
+def value_count(value, list):
+    print("Pizza time")
+
+if __name__ == "__main__":
+    value_count(1, [4, 3, 2, 1])
+    value_count(1, [1, 2, 3, 1, 2, 2])
+    value_count(2, [1, 2, 3, 1, 2, 2])
