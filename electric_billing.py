1ab1eb4b3ab0f3dee2c1c6347745ee1e1be88ec0
Shane Flynn <sflynn248@gmail.com>
2021-10-07 12:19:49 -0500

    cleaned output

--- electric_billing.py
+++ electric_billing.py
@@ -3,12 +3,14 @@ import csv
 
 def read_csv():
     file_path = input("Enter the filepath of the CSV file: ")
+    print()
     customerID = []
     kwhUsed = []
     with open(file_path, newline='') as csv_file:
         cust_reader = csv.reader(csv_file)
         for cust_info in cust_reader:
-            customerID, kwhUsed = cust_info
+            customerID.append(cust_info[0])
+            kwhUsed.append(int(cust_info[1]))
     
     return customerID, kwhUsed
 
@@ -34,17 +36,23 @@ def calculate_cost(kwhUsed):
     return cost
 
 def display_table(customerID, kwhUsed):
-    print("Customer ID\tkWh Used\tCharge\n\
-        -------------------------------")
+    print("{:<13} {:<13} {:<13}".format("Customer ID", "kWh Used", "Charge"))
+    print("--------------------------------------")
     total_power_usage = 0
     total_charges = 0
+    num_customers = len(customerID)
 
-    for i in range(0,len(customerID)):
+    for i in range(0,num_customers):
         cost = calculate_cost(kwhUsed[i])
         total_power_usage += kwhUsed[i]
         total_charges += cost
-        print(f"{customerID[i]}\t{kwhUsed[i]}\t{cost}")
-
+        print(f"{customerID[i]:<13} {kwhUsed[i]:<13} {cost:<13,.2f}")
+    
+    print("======================================")
+    print(f'{"Total number of customers:":<27} {num_customers}')
+    print(f'{"Total power usage (kWh):":<27} {total_power_usage}')
+    print(f'{"Total charges:":<27} {total_charges:.2f}')
 
 if __name__ == '__main__':
-    read_csv()
+    cid, kwh = read_csv()
+    display_table(cid, kwh)
